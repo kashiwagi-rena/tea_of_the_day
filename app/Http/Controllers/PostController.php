a628186b73af20d0525eaf55cfa066508d1815fb
kashiwagi-rena <azisai07@gmail.com>
2023-08-07 11:16:01 +0900

    画像保存時に、画像ファイルの先頭に保存日時（秒数まで）が入るように工夫した #3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,11 +39,12 @@ class PostController extends Controller
         $post->user_id=auth()->user()->id;
 
         if (request('image')){
-            $name = request()->file('image')->getClientOriginalName();
+            $original = request()->file('image')->getClientOriginalName();
+            $name = date('Ymd_His').'_'.$original;
             request()->file('image')->move('storage/images', $name);
             $post->image = $name;
         }
-        
+
         $post->save();
         return redirect()->route('post.create')->with('message','投稿を作成しました');
     }
