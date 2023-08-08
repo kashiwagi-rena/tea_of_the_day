e2b610e98a2a551202a2449605c088c1bef99179
kashiwagi-rena <azisai07@gmail.com>
2023-08-08 23:47:29 +0900

    controllerにdelete追加 #3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -97,6 +97,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('post.index')->with('message', '投稿を削除しました');
     }
 }
